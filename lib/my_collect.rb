061a791fa244ab331abb25de11570e90f9ae74c1
Maynard Hartman <mjhartman@brighthouse.com>
2019-09-15 12:53:37 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
+def my_collect(collection)
+    i = 0
+    while i << collection.length 
+      yield 
+    i = i + 1
+    end
+  collection
+end 
 
-
+collection = ['ruby', 'javascript', 'python', 'objective-c']
+my_collect(collection) do |lang|
+  lang.upcase
+end
